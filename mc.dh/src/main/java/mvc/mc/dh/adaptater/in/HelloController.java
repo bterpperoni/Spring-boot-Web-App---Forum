145e757fcddd5862cd07371263a078490aeae87a
Donovan <donovanhoyois@hotmail.com>
2022-11-14 09:05:31 +0100

    class Story + port in

--- mc.dh/src/main/java/mvc/mc/dh/adaptater/in/HelloController.java
+++ mc.dh/src/main/java/mvc/mc/dh/adaptater/in/HelloController.java
@@ -1,4 +1,4 @@
-package mvc.mc.dh.springboot;
+package mvc.mc.dh.adaptater.in;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +9,10 @@ public class HelloController {
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+    @GetMapping("/test")
+    public String test(){
+        return "test";
     }
 
 }
