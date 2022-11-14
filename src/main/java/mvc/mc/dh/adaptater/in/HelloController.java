d6a22509a4ad7ab8d2bfe8a9f40c192f035729c9
Donovan <donovanhoyois@hotmail.com>
2022-11-14 11:52:07 +0100

    Dependances thymeleaf + template test + controller

--- src/main/java/mvc/mc/dh/adaptater/in/HelloController.java
+++ src/main/java/mvc/mc/dh/adaptater/in/HelloController.java
@@ -1,14 +1,18 @@
 package mvc.mc.dh.adaptater.in;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index";
     }
     @GetMapping("/test")
     public String test(){
