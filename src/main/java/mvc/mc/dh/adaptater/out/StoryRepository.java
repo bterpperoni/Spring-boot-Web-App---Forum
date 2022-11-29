d7f9275b8e2d1aebbfb9bea2a34acdc40b71bb84
Donovan <donovanhoyois@hotmail.com>
2022-11-29 13:18:41 +0100

    link to the local database, dependencies postgresql

--- src/main/java/mvc/mc/dh/adaptater/out/StoryRepository.java
+++ src/main/java/mvc/mc/dh/adaptater/out/StoryRepository.java
@@ -1,15 +1,8 @@
 package mvc.mc.dh.adaptater.out;
 
-//import mvc.mc.dh.adaptater.out.StoryJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface StoryRepository extends JpaRepository<StoryJpaEntity, Long>{
-    /*
-    List<StoryJpaEntity> findAll();
-    StoryJpaEntity findById(int id);
-
-     */
 }
