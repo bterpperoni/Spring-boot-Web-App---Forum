c624d76b6d088981554a0c7f0dec25b0b276204c
bterpperoni <maxime.curon@std.heh.be>
2022-12-05 17:05:15 +0100

    Setting up view for storyID, storyList and storyCreate

--- src/main/java/mvc/mc/dh/adaptater/in/StoryController.java
+++ src/main/java/mvc/mc/dh/adaptater/in/StoryController.java
@@ -29,27 +29,6 @@ public class StoryController {
         return "storyList";
     }
 
-    // Test page
-    @GetMapping("/test")
-    public String test(Model model){
-        /*
-        Story newStory = new Story(0, "test", null, null, null);
-        model.addAttribute("story", newStory);
-         */
-        Story newStory = new Story(0, "Titre2", "Contenu2", LocalDateTime.now(), LocalDateTime.now());
-        storyUseCase.addStory(newStory);
-        return "test";
-    }
-
-    // Return the story List view
-    @GetMapping("/stories")
-    public String viewStories(Model model){
-        storyList = storyUseCase.getStories();
-        // The name in "" will be used by thymeleaf to provide view
-        model.addAttribute("storyList",storyList);
-        return "storyList";
-    }
-
     @GetMapping("/story/{id}")
     public String viewStory(@PathVariable("id") Long ID,Model model){
         Story storyID = storyUseCase.getStory(ID);
